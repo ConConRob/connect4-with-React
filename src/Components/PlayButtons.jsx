1614f8943f673d4883f2a48963208429a2dbd164
conconrob <connortprobertson@gmail.com>
2019-02-14 21:21:10 +0000

    half done check if win function

--- src/Components/PlayButtons.jsx
+++ src/Components/PlayButtons.jsx
@@ -5,11 +5,12 @@ export default function PlayButtons({playATurn, possiblePlays}) {
         playATurn(Column);
     }
     return (
+        
         <div className="play-buttons">
         {
             possiblePlays.map((possiblePlay, index) => {
                 return (
-                    <button onClick={()=>clickAction(index)} key={index} className={`play-button ${!possiblePlay&& 'disabled'}`}>{`Play Column ${index+1}`}</button>
+                    <button onClick={()=>clickAction(index)} key={index} className={`play-button`} disabled={ !possiblePlay } >{`Play Column ${index+1}`}</button>
                 )
             })
         }
