99ed03923a44de9a2c73543175ca484c2921ac3f
conconrob <connortprobertson@gmail.com>
2019-02-22 17:38:31 +0000

    comp finds if spot can make win play and returns that value

--- src/Components/checkersBrain.js
+++ src/Components/checkersBrain.js
@@ -61,11 +61,129 @@ export default function decideOnPlay(boardState, possiblePlays) {
         }
         return false;
     }
+    // for every  spot calculate a score depending on  how many in a row
+    // if can't play spot give a score of -1 
+    const valueOfPlaysBoard = boardState.map((row, yIndex) => 
+        row.map((spot, xIndex) => {        
+            //check if spot is filled
+            if (spot!==0){
+                return -1
+            }
+            //check if spot doesn't have something under it 
+            if (isDown(yIndex, xIndex,0)){
+                return -1
+            }
+            // DONE filtering out unavailable plays
+            // check amount in line in each direction
+            let leftMatch = 0; 
+            if(isLeft(yIndex,xIndex,playerNumber)){
+                leftMatch=1;
+                if(isLeft(yIndex,xIndex-1,playerNumber)){
+                    leftMatch=2;
+                    if(isLeft(yIndex,xIndex-2,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            let rightMatch =0;
+            if(isRight(yIndex,xIndex,playerNumber)){
+                rightMatch=1;
+                if(isRight(yIndex,xIndex+1,playerNumber)){
+                    rightMatch=2;
+                    if(isRight(yIndex,xIndex+2,playerNumber)){
+                        return 999;
+                    }
+                }
+            } 
+            if(rightMatch+leftMatch >=3){
+                return 999;
+            }
+            let downMatch=0;
+            if(isDown(yIndex,xIndex,playerNumber)){
+                downMatch=1;
+                if(isDown(yIndex+1,xIndex,playerNumber)){
+                    downMatch=2;
+                    if(isDown(yIndex+2,xIndex,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            // positive slope match 
+            let upRightMatch = 0;
+
+            if(isUpRight(yIndex,xIndex,playerNumber)){
+                upRightMatch=1;
+                if(isUpRight(yIndex-1,xIndex+1,playerNumber)){
+                    upRightMatch=2;
+                    if(isUpRight(yIndex-2,xIndex+2,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            let downLeftMatch = 0;            
+            if(isDownLeft(yIndex,xIndex,playerNumber)){
+                downLeftMatch=1;
+                if(isDownLeft(yIndex+1,xIndex-1,playerNumber)){
+                    downLeftMatch=2;
+                    if(isDownLeft(yIndex+2,xIndex-2,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            if(upRightMatch+downLeftMatch >=3){
+                return 999;
+            }
+            //negative slope
+            let upLeftMatch = 0;
+            if(isUpLeft(yIndex,xIndex,playerNumber)){
+                upLeftMatch=1;
+                if(isUpLeft(yIndex-1,xIndex-1,playerNumber)){
+                    upLeftMatch=2;
+                    if(isUpLeft(yIndex-2,xIndex-2,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            let downRightMatch = 0;            
+            if(isDownRight(yIndex,xIndex,playerNumber)){
+                downRightMatch=1;
+                if(isDownRight(yIndex+1,xIndex+1,playerNumber)){
+                    downRightMatch=2;
+                    if(isDownRight(yIndex+2,xIndex+2,playerNumber)){
+                        return 999;
+                    }
+                }
+            }
+            if(upLeftMatch+downRightMatch >=3){
+                return 999;
+            }
+            // now try to calculate the value of it bases on how many 
+            return 0;
+        }) 
+    )
+    
+    // ge the index of the best play 
+    let max= 0;
+    let maxXIndex;
+    valueOfPlaysBoard.forEach((row, yIndex) => 
+        row.forEach((spot, xIndex) => {
+              //check if spot is playable
+              if (spot<0){
+                return
+            }
+            // check if highest score to play
+            if(spot>=max){
+                max=spot;
+                maxXIndex=xIndex;
+            }
+        })
+        )      
+    console.log(maxXIndex)
 
     // check if you can get have a 4 in a line win
-    possiblePlays.forEach((canPlay, rowIndex)=>{
-        console.log(isDownLeft(4,rowIndex, opponentPlayerNumber), rowIndex)
-    })
+    // possiblePlays.forEach((canPlay, rowIndex)=>{
+    //     console.log(isDownLeft(4,rowIndex, opponentPlayerNumber), rowIndex)
+    // })
     // check  if you need to stop other player's 4 in a line win
     // check if you need to stop other player's 2 turn win 
     // check if you can play for a 2 turn win 
