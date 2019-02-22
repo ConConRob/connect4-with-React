701f9dbb16f09c74e5471cab454c92b6c6e92ff9
conconrob <connortprobertson@gmail.com>
2019-02-22 18:12:37 +0000

    it is playable

--- src/Components/checkersBrain.js
+++ src/Components/checkersBrain.js
@@ -67,108 +67,125 @@ export default function decideOnPlay(boardState, possiblePlays) {
         row.map((spot, xIndex) => {        
             //check if spot is filled
             if (spot!==0){
-                return -1
+                return -99999
             }
             //check if spot doesn't have something under it 
             if (isDown(yIndex, xIndex,0)){
-                return -1
+                return -99999
             }
             // DONE filtering out unavailable plays
-            // check amount in line in each direction
-            let leftMatch = 0; 
-            if(isLeft(yIndex,xIndex,playerNumber)){
-                leftMatch=1;
-                if(isLeft(yIndex,xIndex-1,playerNumber)){
-                    leftMatch=2;
-                    if(isLeft(yIndex,xIndex-2,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            let rightMatch =0;
-            if(isRight(yIndex,xIndex,playerNumber)){
-                rightMatch=1;
-                if(isRight(yIndex,xIndex+1,playerNumber)){
-                    rightMatch=2;
-                    if(isRight(yIndex,xIndex+2,playerNumber)){
-                        return 999;
-                    }
-                }
-            } 
-            if(rightMatch+leftMatch >=3){
-                return 999;
-            }
-            let downMatch=0;
-            if(isDown(yIndex,xIndex,playerNumber)){
-                downMatch=1;
-                if(isDown(yIndex+1,xIndex,playerNumber)){
-                    downMatch=2;
-                    if(isDown(yIndex+2,xIndex,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            // positive slope match 
-            let upRightMatch = 0;
+            // get value base on computers players
 
-            if(isUpRight(yIndex,xIndex,playerNumber)){
-                upRightMatch=1;
-                if(isUpRight(yIndex-1,xIndex+1,playerNumber)){
-                    upRightMatch=2;
-                    if(isUpRight(yIndex-2,xIndex+2,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            let downLeftMatch = 0;            
-            if(isDownLeft(yIndex,xIndex,playerNumber)){
-                downLeftMatch=1;
-                if(isDownLeft(yIndex+1,xIndex-1,playerNumber)){
-                    downLeftMatch=2;
-                    if(isDownLeft(yIndex+2,xIndex-2,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            if(upRightMatch+downLeftMatch >=3){
-                return 999;
-            }
-            //negative slope
-            let upLeftMatch = 0;
-            if(isUpLeft(yIndex,xIndex,playerNumber)){
-                upLeftMatch=1;
-                if(isUpLeft(yIndex-1,xIndex-1,playerNumber)){
-                    upLeftMatch=2;
-                    if(isUpLeft(yIndex-2,xIndex-2,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            let downRightMatch = 0;            
-            if(isDownRight(yIndex,xIndex,playerNumber)){
-                downRightMatch=1;
-                if(isDownRight(yIndex+1,xIndex+1,playerNumber)){
-                    downRightMatch=2;
-                    if(isDownRight(yIndex+2,xIndex+2,playerNumber)){
-                        return 999;
-                    }
-                }
-            }
-            if(upLeftMatch+downRightMatch >=3){
-                return 999;
-            }
-            // now try to calculate the value of it bases on how many 
-            return 0;
+            const valueBasedOnMe = getValueOfSpot(xIndex, yIndex, playerNumber, 1.5);
+            const valueBaseOnOtherPlayer =getValueOfSpot(xIndex, yIndex, opponentPlayerNumber, 1);
+            const valueBaseOnUnplayedSpace = getValueOfSpot(xIndex, yIndex, 0 , 0.5);
+            return   valueBasedOnMe + valueBaseOnOtherPlayer;
         }) 
     )
+
+    function getValueOfSpot(xIndex, yIndex, checkFor, multiplier){
+        let leftMatch = 0; 
+        if(isLeft(yIndex,xIndex,checkFor)){
+            leftMatch=1;
+            if(isLeft(yIndex,xIndex-1,checkFor)){
+                leftMatch=2;
+                if(isLeft(yIndex,xIndex-2,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        let rightMatch =0;
+        if(isRight(yIndex,xIndex,checkFor)){
+            rightMatch=1;
+            if(isRight(yIndex,xIndex+1,checkFor)){
+                rightMatch=2;
+                if(isRight(yIndex,xIndex+2,checkFor)){
+                    return 999;
+                }
+            }
+        } 
+        const horizontal = rightMatch+leftMatch;
+        if( horizontal >=3){
+            return 999;
+        }
+        let vertical=0;
+        if(isDown(yIndex,xIndex,checkFor)){
+            vertical=1;
+            if(isDown(yIndex+1,xIndex,checkFor)){
+                vertical=2;
+                if(isDown(yIndex+2,xIndex,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        // positive slope match 
+        let upRightMatch = 0;
+
+        if(isUpRight(yIndex,xIndex,checkFor)){
+            upRightMatch=1;
+            if(isUpRight(yIndex-1,xIndex+1,checkFor)){
+                upRightMatch=2;
+                if(isUpRight(yIndex-2,xIndex+2,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        let downLeftMatch = 0;            
+        if(isDownLeft(yIndex,xIndex,checkFor)){
+            downLeftMatch=1;
+            if(isDownLeft(yIndex+1,xIndex-1,checkFor)){
+                downLeftMatch=2;
+                if(isDownLeft(yIndex+2,xIndex-2,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        const positiveSlope= upRightMatch+downLeftMatch;
+        if( positiveSlope >=3){
+            return 999;
+        }
+        //negative slope
+        let upLeftMatch = 0;
+        if(isUpLeft(yIndex,xIndex,checkFor)){
+            upLeftMatch=1;
+            if(isUpLeft(yIndex-1,xIndex-1,checkFor)){
+                upLeftMatch=2;
+                if(isUpLeft(yIndex-2,xIndex-2,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        let downRightMatch = 0;            
+        if(isDownRight(yIndex,xIndex,checkFor)){
+            downRightMatch=1;
+            if(isDownRight(yIndex+1,xIndex+1,checkFor)){
+                downRightMatch=2;
+                if(isDownRight(yIndex+2,xIndex+2,checkFor)){
+                    return 999;
+                }
+            }
+        }
+        const negativeSlope = upLeftMatch+downRightMatch
+        if(negativeSlope >= 3){
+            return 999;
+        }
+        // now try to calculate the value of it bases on how many in row in each direction
+        //
+        const playValue= vertical*multiplier + horizontal*multiplier + positiveSlope*multiplier + negativeSlope*multiplier;
+        return playValue;
+    }
     
-    // ge the index of the best play 
+    
+    
+    
+    console.table(valueOfPlaysBoard)
+    // get the index of the best play 
     let max= 0;
     let maxXIndex;
     valueOfPlaysBoard.forEach((row, yIndex) => 
         row.forEach((spot, xIndex) => {
               //check if spot is playable
-              if (spot<0){
+              if (spot === -99999){
                 return
             }
             // check if highest score to play
@@ -191,7 +208,7 @@ export default function decideOnPlay(boardState, possiblePlays) {
     // make set cases for early game and mix in random to make it so it doesn't follow a pattern
 
     // make a random play
-    return Math.floor((Math.random()*7));
+    return maxXIndex;
 }
 
 
