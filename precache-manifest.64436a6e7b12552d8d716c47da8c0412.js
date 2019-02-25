c750fc9f59c0037f352299da5794aa79ef4084dc
conconrob <connortprobertson@gmail.com>
2019-02-25 14:25:11 +0000

    Updates

---
+++ precache-manifest.64436a6e7b12552d8d716c47da8c0412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58dd5d7eaf16d733b11a",
+    "url": "/connect4-with-React/static/js/runtime~main.58dd5d7e.js"
+  },
+  {
+    "revision": "3293512503b9c70a074f",
+    "url": "/connect4-with-React/static/js/main.32935125.chunk.js"
+  },
+  {
+    "revision": "9054da009dc364c58e6c",
+    "url": "/connect4-with-React/static/js/2.9054da00.chunk.js"
+  },
+  {
+    "revision": "3293512503b9c70a074f",
+    "url": "/connect4-with-React/static/css/main.796b777d.chunk.css"
+  },
+  {
+    "revision": "09e4772932648a5ba939aaa89cc8e2dc",
+    "url": "/connect4-with-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "conconrob.github.io/connect4-with-React/precache-manifest.3357cfbfd3a57a8e00441c61601843dc.js"
+  "/connect4-with-React/precache-manifest.64436a6e7b12552d8d716c47da8c0412.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("conconrob.github.io/connect4-with-React/index.html", {
+workbox.routing.registerNavigationRoute("/connect4-with-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
